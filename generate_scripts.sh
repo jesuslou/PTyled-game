07b99c695e65f07e49512faaf0914050e93aee17
jesuslou <loucocito@gmail.com>
2017-10-03 20:38:57 +0200

    - Added two new autogenerated files

---
+++ generate_scripts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python "C:\my_projects\PTyledGame\dependencies\LouEngine\build\scripts\generate_base_scripts.py" --folder "C:\my_projects\PTyledGame"
+
+read -p "Press any key to continue..."
---
+++ run_resource_pipeline.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python "C:\my_projects\PTyledGame\dependencies\LouEngine\tools\scripts\PackingPipeline.py" --packs_file "C:\my_projects\PTyledGame\res\packerTool\packsDefinition.json"
+
+read -p "Press any key to continue..."
